fcd16b0c918a9562277bfb99e6cfb18634aa2e89
taksato-redhat <taksato@redhat.com>
2017-01-04 04:33:03 +0900

    HelloResource パス変更

--- hello/web/src/main/java/local/hello/web/rs/HelloResource.java
+++ hello/web/src/main/java/local/hello/web/rs/HelloResource.java
@@ -4,13 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-@Path("/hello")
 public interface HelloResource {
 
 	@GET
+	@Path("/hello")
 	String hello(@QueryParam("name") String name);
 	
 	@GET
+	@Path("/bye")
 	String bye(@QueryParam("name") String name);
 
 }
